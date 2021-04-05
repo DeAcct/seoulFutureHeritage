5fdd658a799ae17a1ef90214cba593be2e8e2a33
Hyeon Seong LEE <dynamic1117@gmail.com>
2021-04-05 22:30:26 +0900

    PWA experimental apply
    PWA experimental apply

---
+++ js/serviceWorker.js
@@ -0,0 +1,16 @@
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('./serviceWorker.js').then(function(reg){
+        reg.addEventListener('updatefound', function(){
+            newWorker = reg.installing;
+            newWorker.addEventListener('statechange', function(){
+                switch(newWorker.state){
+                    case 'installed':
+                        if (navigator.serviceWorker.controller){
+                            showUpdateBar();
+                        };
+                    break
+                }
+            })
+        })
+    })
+}
